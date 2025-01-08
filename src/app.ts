84c37982b1730a9e843b8f47a12ffd5973be9266
Guilherme Maciel <guilhermemcl1503@gmail.com>
2025-01-08 06:48:13 -0300

    Add user and gym check-in models with relationships; implement user registration endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,24 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.post('/user', async ( request, reply ) => {
+  const registerBodyschema =  z.object({ 
+    name:z.string(),
+    email:z.string().email(),
+    password:z.string().min(6)
+   })
+   const { name, email, password } = registerBodyschema.parse(request.body)
 
+   await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password
+    }
+  })
 
-prisma.user.create({
-  data: {
-    name: 'Diego Fernandes',
-    email: 'diego@rocketsaeet.com.br',
-  },
+  return reply.status(201).send()
 })
-
-// ORM - Object Relational Mapper
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import { app } from './app'
 import { env } from './env'
 
-app
-  .listen({
+app.listen({
     host: '0.0.0.0',
     port: env.PORT,
   })
   .then(() => {
-    console.log('🚀 HTTP Server Running!')
+    console.log(`🚀 HTTP Server Running on port ${env.PORT}!`)
   })
